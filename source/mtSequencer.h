d51012f44dfa27a43b1b65b448fca9ee4b0671d4
shuler <shuler@interia.eu>
2018-12-13 11:27:19 +0100

    branch init

---
+++ source/mtSequencer.h
@@ -0,0 +1,15 @@
+/*
+ * mtSequencer.h
+ *
+ *  Created on: 13 gru 2018
+ *      Author: krzys
+ */
+
+#ifndef SOURCE_MTSEQUENCER_H_
+#define SOURCE_MTSEQUENCER_H_
+
+
+
+
+
+#endif /* SOURCE_MTSEQUENCER_H_ */
