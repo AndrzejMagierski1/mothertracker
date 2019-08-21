7627ae739ebe6b73ae21d72b5e825f19a39a2435
Andrzej Magierski <andrzej@polyend.com>
2019-08-21 15:59:36 +0200

    mtAudio
    - podpięcie mixera reverbów do ampów

--- source/mtAudioEngine.cpp
+++ source/mtAudioEngine.cpp
@@ -74,14 +74,14 @@ AudioConnection          connect38(&amp[5], 0, &mixerR, 5);
 AudioConnection          connect39(&amp[6], 0, &mixerR, 6);
 AudioConnection          connect40(&amp[7], 0, &mixerR, 7);
 
-AudioConnection          connect41(&envelopeAmp[0], 0, &mixerReverb, 0);
-AudioConnection          connect42(&envelopeAmp[1], 0, &mixerReverb, 1);
-AudioConnection          connect43(&envelopeAmp[2], 0, &mixerReverb, 2);
-AudioConnection          connect44(&envelopeAmp[3], 0, &mixerReverb, 3);
-AudioConnection          connect45(&envelopeAmp[4], 0, &mixerReverb, 4);
-AudioConnection          connect46(&envelopeAmp[5], 0, &mixerReverb, 5);
-AudioConnection          connect47(&envelopeAmp[6], 0, &mixerReverb, 6);
-AudioConnection          connect48(&envelopeAmp[7], 0, &mixerReverb, 7);
+AudioConnection          connect41(&amp[0], 0, &mixerReverb, 0);
+AudioConnection          connect42(&amp[1], 0, &mixerReverb, 1);
+AudioConnection          connect43(&amp[2], 0, &mixerReverb, 2);
+AudioConnection          connect44(&amp[3], 0, &mixerReverb, 3);
+AudioConnection          connect45(&amp[4], 0, &mixerReverb, 4);
+AudioConnection          connect46(&amp[5], 0, &mixerReverb, 5);
+AudioConnection          connect47(&amp[6], 0, &mixerReverb, 6);
+AudioConnection          connect48(&amp[7], 0, &mixerReverb, 7);
 
 AudioConnection          connect49(&mixerReverb,&reverb);
 
@@ -147,6 +147,9 @@ void audioEngine::init()
 	audioShield.lineInLevel(3);
 	limiter[0].begin(30000, 300, 20);
 	limiter[1].begin(30000, 300, 20);
+
+	setReverbDamping(mtProject.values.reverbDamping);
+	setReverbRoomsize(mtProject.values.reverbRoomSize);
 	for(int i=0;i<8; i++)
 	{
 		instrumentPlayer[i].init(&playMem[i],&envelopeFilter[i],&filter[i],&envelopeAmp[i], &amp[i], i, &lfoAmp[i],&lfoFilter[i],&lfoPitch[i]);
@@ -230,6 +233,7 @@ void audioEngine::setReverbRoomsize(uint8_t value)
 
 void audioEngine::setReverbDamping(uint8_t value)
 {
+	if(value > 100) value == 100;
 	reverb.damping(value/100.0);
 }
 
