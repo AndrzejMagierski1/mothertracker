bbab1ca0dfc7ae06f82edcb7a5bc6ff53dfa4dec
Grzegorz <grzegorz@polyend.com>
2019-12-04 15:08:26 +0100

    Hold na insert/delete w patternie

--- interface/funct/fPatternEdit.cpp
+++ interface/funct/fPatternEdit.cpp
@@ -1869,9 +1869,8 @@ static  uint8_t functRecAction()
 
 static uint8_t functInsertHome(uint8_t state)
 {
-	if (state == buttonPress)
+	if (state == buttonPress || state == buttonHold)
 	{
-
 		if (PTE->editMode == 1)
 		{
 			fileManager.storePatternUndoRevision();
@@ -1976,10 +1975,8 @@ static uint8_t functCopyPaste(uint8_t state)
 }
 static uint8_t functDeleteBackspace(uint8_t state)
 {
-
-	if (state == buttonPress)
+	if (state == buttonPress || state == buttonHold)
 	{
-
 		if (PTE->editMode == 1)
 		{
 			fileManager.setPatternChangeFlag(mtProject.values.actualPattern);
@@ -1988,21 +1985,24 @@ static uint8_t functDeleteBackspace(uint8_t state)
 			// backspace
 			if (tactButtons.isButtonPressed(interfaceButtonShift))
 			{
-				sendSelection();
-				sequencer.backspace();
-				if (isMultiSelection())
+				if(state == buttonPress)
 				{
-					if (PTE->trackerPattern.selectStartStep > 0)
+					sendSelection();
+					sequencer.backspace();
+					if (isMultiSelection())
 					{
-						PTE->trackerPattern.selectStartStep--;
-						PTE->trackerPattern.selectEndStep--;
-						PTE->trackerPattern.actualStep--; // zmiana pozycji kursora
+						if (PTE->trackerPattern.selectStartStep > 0)
+						{
+							PTE->trackerPattern.selectStartStep--;
+							PTE->trackerPattern.selectEndStep--;
+							PTE->trackerPattern.actualStep--; // zmiana pozycji kursora
+						}
 					}
-				}
-				else
-				{
-					if (PTE->trackerPattern.actualStep > 0)
-						PTE->trackerPattern.actualStep--; // zmiana pozycji kursora
+					else
+					{
+						if (PTE->trackerPattern.actualStep > 0)
+							PTE->trackerPattern.actualStep--; // zmiana pozycji kursora
+					}
 				}
 			}
 			// DELETE
