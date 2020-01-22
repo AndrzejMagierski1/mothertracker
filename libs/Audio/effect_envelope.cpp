2a7a7b8c65da70d9ab991b3bb84408e39c33d834
Andrzej2000 <andrzej@polyend.com>
2020-01-22 15:41:00 +0100

    usprawnianie envelopa

--- libs/Audio/effect_envelope.cpp
+++ libs/Audio/effect_envelope.cpp
@@ -116,67 +116,26 @@ void AudioEffectEnvelope::update(void)
 			if (state == STATE_ATTACK)
 			{
 				count = hold_count;
-				if (count > 0)
-				{
-					state = STATE_HOLD;
-					mult_hires = 0x40000000;
-					inc_hires = 0;
-				}
-				else
-				{
-					state = STATE_DECAY;
-					count = decay_count;
-					inc_hires = (sustain_mult - 0x40000000) / (int32_t)count;
-				}
+				state = STATE_HOLD;
+				mult_hires = 0x40000000;
+				inc_hires = 0;
 				continue;
 			}
 			else if (state == STATE_HOLD)
 			{
 				state = STATE_DECAY;
 				count = decay_count;
-				inc_hires = (sustain_mult - 0x40000000) / (int32_t)count;
+				inc_hires = count ? (sustain_mult - 0x40000000) / (int32_t)count : 0 ;
 				continue;
 			}
 			else if (state == STATE_DECAY)
 			{
 				if(loopFlag && pressedFlag)
 				{
-					// tutaj pełna obsługa przypadu gdzie wszystko ustawione jest na 0 (attack decay release)
-					// inne stany wrzucą się ostatecznie w ten i tu zostanie ustawiony sustain
 					count = release_count;
-					if(count > 0)
-					{
-						state = STATE_RELEASE;
-						inc_hires = (-mult_hires) / (int32_t)count;
-						continue;
-					}
-					else
-					{
-						count = attack_count;
-						if(count > 0)
-						{
-							state = STATE_ATTACK;
-							inc_hires = 0x40000000 / (int32_t)count;
-							continue;
-						}
-						else
-						{
-							count = decay_count;
-							if(count > 0)
-							{
-								state = STATE_DECAY;
-								inc_hires = (sustain_mult - 0x40000000) / (int32_t)count;
-								continue;
-							}
-							else
-							{
-								state = STATE_SUSTAIN;
-								count = 0xFFFF;
-								mult_hires = sustain_mult;
-								inc_hires = 0;
-							}
-						}
-					}
+					state = STATE_RELEASE;
+					inc_hires = count ? (-mult_hires) / (int32_t)count : 0;
+					continue;
 				}
 				else
 				{
@@ -196,20 +155,9 @@ void AudioEffectEnvelope::update(void)
 				{
 					//powinienem dać delay state, ale nie jest uzywany w trackerze, a moze tworzyc dodatkowe problemy
 					count = attack_count;
-					if(count > 0)
-					{
-						state = STATE_ATTACK;
-						inc_hires = 0x40000000 / (int32_t)count;
-						continue;
-					}
-					else
-					{
-						count = decay_count;
-						state = STATE_DECAY;
-						inc_hires = (sustain_mult - 0x40000000) / (int32_t)count;
-						continue;
-					}
-
+					state = STATE_ATTACK;
+					inc_hires = count ? 0x40000000 / (int32_t)count : 0;
+					continue;
 				}
 				else
 				{
@@ -230,23 +178,16 @@ void AudioEffectEnvelope::update(void)
 			{
 				mult_hires = 0;
 				count = delay_count;
-				if (count > 0)
-				{
-					state = STATE_DELAY;
-					inc_hires = 0;
-				}
-				else
-				{
-					state = STATE_ATTACK;
-					count = attack_count;
-					inc_hires = 0x40000000 / (int32_t)count;
-				}
+				state = STATE_DELAY;
+				inc_hires = 0;
+				continue;
+
 			}
 			else if (state == STATE_DELAY)
 			{
 				state = STATE_ATTACK;
 				count = attack_count;
-				inc_hires = 0x40000000 / count;
+				inc_hires = count ?  0x40000000 / count : 0;
 				continue;
 			}
 		}
